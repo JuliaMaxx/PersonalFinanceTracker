83c040d1dd6edebd1fec8e84ffa1c0a7f68a9158
JuliaMaxx <jjuliamaxxx@gmail.com>
2026-05-12 22:43:54 +0200

    update to not use IAM user

--- personal_finance_tracker/settings.py
+++ personal_finance_tracker/settings.py
@@ -144,8 +144,6 @@ LOGIN_REDIRECT_URL = f"/finance_tracker/{current_month}/"
 if USE_S3:
     INSTALLED_APPS += ["storages"]
 
-    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
     AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME")
     AWS_S3_CUSTOM_DOMAIN = os.environ.get(
